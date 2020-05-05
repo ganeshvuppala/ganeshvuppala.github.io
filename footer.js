6382af4b657b9785d939324a3573bf6bc271a1b9
Ganesh Vuppala <64791080+ganeshvuppala@users.noreply.github.com>
2020-05-06 01:16:47 +0530

    Added the files for the first time
    Here files have the structure, but have an empty body
    This is the first commit to Git

---
+++ footer.js
@@ -0,0 +1,14 @@
+document.write(`
+</div>
+
+</div>
+<div id="prefooter">
+
+</div>
+<div id="footer">
+<div class="padding"> Copyright &copy; 2020 ganeshvuppala.github.io | Design:  <a href="http://www.instagram.com/rajashekar_kasturi" target="_blank">Rajashekar Kasturi </a> | <a href="https://www.linkedin.com/in/rajashekar-kasturi-49b837126/" target="_blank">Contact</a>  </div>
+</div>
+</div>
+</body>
+</html>
+`);
---
+++ header.js
@@ -0,0 +1,32 @@
+document.write(`
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<title>Ganesh Vuppala</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<style type="text/css" media="all">
+@import "images/style.css";
+</style>
+</head>
+<body>
+<div class="content">
+  <div id="header">
+    <div class="title">
+    <h1><a href="index.html">Ganesh Vuppala</a></h1>
+    </div>
+  </div>
+  <div class="leftmenu">
+    <div class="nav">
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="articles.html">Articles</a></li>
+        <li><a href="gallery.html">Gallery</a></li>
+        <li><a href="archives.html">Archives</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+  <div id="main">
+    <div class="center">
+`);
